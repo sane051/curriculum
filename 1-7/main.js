27e1d6b35464d7d353d85382386526ada3c0e965
sane051 <99936825+sane051@users.noreply.github.com>
2023-05-12 08:56:43 +0900

    1－7

---
+++ 1-7/main.js
@@ -0,0 +1,22 @@
+let scores= [10,15,20,25];
+let i = 0;
+while(i<=3){
+  
+    if(i%2==0){
+    console.log(scores[i]+"は偶数です");
+   
+    }
+    i++;
+}
+
+
+
+
+
+let car={
+    gass:20.5,
+    num:1234,
+};
+
+console.log(`ガソリンは${car.gass}です`);
+console.log(`ナンバーは${car.num}です`);
